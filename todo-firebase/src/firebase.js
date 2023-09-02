591d8a15f66b389f05b4ca9c20ee359f9281e60d
Jabir Mustahid <jabirm2002@gmail.com>
2023-09-02 12:53:58 -0400

    Update firebase.js

--- todo-firebase/src/firebase.js
+++ todo-firebase/src/firebase.js
@@ -6,8 +6,21 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 
+const apiKey = process.env.REACT_APP_APIKEY
+const authDomain = process.REACT_APP_AUTHDOMAIN
+const projectId = process.env.REACT_APP_PROJECTID
+const storageBucket = process.env.REACT_APP_STORAGEBUCKET
+const messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID
+const appId = process.env.REACT_APP_APPID
 
-
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
